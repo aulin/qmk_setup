53ff716e8683dd22e7647ded0cae9094d99ec816
Johan Aulin <johan.aulin@gmail.com>
2024-01-11 21:50:21 +0100

    Version 8
    Finally using qmk itself, with keymap.c

--- keymap.c
+++ keymap.c
@@ -25,10 +25,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
-    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,       KC_T,   KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
-    KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,       KC_G,   KC_H,   KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,       KC_B,   KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_UP,   SC_SENT,
-    KC_LCTL, KC_LGUI, KC_NUBS, KC_LALT, MO(_LOWER), KC_SPC, KC_SPC, MO(_RAISE), KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_ESC,  KC_Q,    KC_W,           KC_E,    KC_R,       KC_T,   KC_Y,   KC_U,       KC_I,    KC_O,    KC_P,    KC_BSPC,
+    KC_TAB,  KC_A,    KC_S,           KC_D,    KC_F,       KC_G,   KC_H,   KC_J,       KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+    KC_LSFT, KC_Z,    KC_X,           KC_C,    KC_V,       KC_B,   KC_N,   KC_M,       KC_COMM, KC_DOT,  KC_UP,   SC_SENT,
+    KC_LCTL, KC_LGUI, MEH_T(KC_NUBS), KC_LALT, MO(_LOWER), KC_SPC, KC_SPC, MO(_RAISE), KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Lower
@@ -43,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12(
-    KC_BSLS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,     KC_CIRC,     KC_AMPR,       KC_ASTR,    KC_LPRN,    KC_RPRN, KC_TRNS,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,       RALT(KC_MINS), RALT(KC_7), RALT(KC_0), KC_SCLN, KC_MINS,
-    KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,      KC_F12,      KC_SLSH,       RALT(KC_8), RALT(KC_9), KC_EQL,  KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TO(_NUMPAD), TO(_NUMPAD), MO(_ADJUST),   KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS
+    KC_BSLS, LSFT(KC_1), LSFT(KC_2), LSFT(KC_3), RALT(KC_4), LSFT(KC_5),  LSFT(KC_6),  LSFT(KC_7),    LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), KC_TRNS,
+    KC_DEL,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,       KC_F6,       RALT(KC_MINS), RALT(KC_7), RALT(KC_0), KC_SCLN,    KC_MINS,
+    KC_TRNS, KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,      KC_F12,      KC_SLSH,       RALT(KC_8), RALT(KC_9), KC_EQL,     KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    TO(_NUMPAD), TO(_NUMPAD), MO(_ADJUST),   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS
 ),
 
 /* Raise
