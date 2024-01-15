48e48a8489f845c23c602596272d6ae24b2b4fff
Johan Aulin <joau@boozt.com>
2024-01-15 10:51:28 +0100

    Add settings for split and autoshift

---
+++ config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/* Select hand configuration */
+#define MASTER_LEFT
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 8
+
+#define AUTO_SHIFT_TIMEOUT 175
+
+//To sync led data on split keyboard
+#define SPLIT_LAYER_STATE_ENABLE
